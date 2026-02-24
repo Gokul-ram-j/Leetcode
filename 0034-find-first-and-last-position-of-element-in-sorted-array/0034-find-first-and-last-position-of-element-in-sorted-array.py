d552c3d2381e722e682a9fd912918f256f69a955
GOKUL RAM J <121298909+Gokul-ram-j@users.noreply.github.com>
2026-02-24 19:14:41 +0530

    Time: 0 ms (100%), Space: 20.7 MB (45.06%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,45 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        res=[-1,-1]
-        curInd=0
 
-        while(curInd<len(nums)):
-            if nums[curInd]==target:
-                res[0]=curInd
-                break
-            curInd+=1
+        def findFirst(nums,target):
 
-        curInd=len(nums)-1
+            left,right=0,len(nums)-1
+            ans=-1
 
-        while(curInd>=0):
-            if nums[curInd]==target:
-                res[-1]=curInd
-                break
-            curInd-=1
-        
-        return res
+            while left<=right:
+                mid=(left+right)//2
+
+                if nums[mid]==target:
+                    ans=mid
+                    right=mid-1
+                elif nums[mid]<target:
+                    left=mid+1
+                else:
+                    right=mid-1
+            
+            return ans
+
+
+        def findLast(nums,target):
+
+            left,right=0,len(nums)-1
+            ans=-1
+
+            while left<=right:
+                mid=(left+right)//2
+
+                if nums[mid]==target:
+                    ans=mid
+                    left=mid+1
+                elif nums[mid]<target:
+                    left=mid+1
+                else:
+                    right=mid-1
+            
+            return ans
+
+        return [findFirst(nums,target),findLast(nums,target)]
 
 
 
