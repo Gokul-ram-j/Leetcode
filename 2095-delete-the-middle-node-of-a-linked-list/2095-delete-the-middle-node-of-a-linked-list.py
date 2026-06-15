9bc7d647f73e6638ba5cdfe2cde9d31123632c39
GOKUL RAM J <121298909+Gokul-ram-j@users.noreply.github.com>
2026-06-15 11:04:43 +0530

    Time: 89 ms (59.13%), Space: 62.4 MB (49.97%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -9,15 +9,11 @@ class Solution:
             return None
         slow=head
         fast=head
+        prev=None
         while(fast and fast.next):
+            prev=slow
             slow=slow.next
             fast=fast.next.next
         
-        prev=None
-        cur=head
-        while(cur!=slow):
-            prev=cur
-            cur=cur.next
-        
-        prev.next=cur.next
+        prev.next=slow.next
         return head
