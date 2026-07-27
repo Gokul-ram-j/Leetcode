dfa142a7f4b923049f0089baa3907e7d8aa38ff9
GOKUL RAM J <121298909+Gokul-ram-j@users.noreply.github.com>
2026-07-27 16:31:09 +0530

    Time: 2878 ms (0%), Space: 19.2 MB (33.33%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def largestInteger(self, n: int, s: int) -> int:
-        
+        if s==0: return 0
         def digitSum(num):
             
             return s==sum([int(i) for i in str(num)])
