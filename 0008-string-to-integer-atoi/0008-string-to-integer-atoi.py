b49473287338b70407975cf91fec8db0c38e8d66
GOKUL RAM J <121298909+Gokul-ram-j@users.noreply.github.com>
2026-07-30 15:45:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (44.19%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,33 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+
+        def cti(s):
+            num = 0
+            for ch in s:
+                num = num * 10 + (ord(ch) - ord('0'))
+            return num
+                
+        res=""
+        sign=""
+        s=s.lstrip()
+        for i in range(len(s)):
+            if s[i].isalpha() or s[i]=='.' or s[i]==" ":
+                break
+            elif s[i].isdigit():
+                res+=s[i]
+            elif s[i]=='-' or s[i]=='+':
+                if res or sign : break
+                sign=s[i]
+        
+        num = -cti(res) if sign == '-' else cti(res)
+
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if num < INT_MIN:
+            return INT_MIN
+        if num > INT_MAX:
+            return INT_MAX
+        return num
+
+       
